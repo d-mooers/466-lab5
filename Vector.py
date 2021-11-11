032d87dc60977c6c07bbe0e95cd6167f86dd9159
Dylan Mooers <dmooers@calpoly.edu>
2021-11-11 11:07:41 -0800

    Added vectorizer

--- Vector.py
+++ Vector.py
@@ -1,11 +1,14 @@
 import numpy as np
+from numpy.lib.function_base import delete
 import pandas as pd
+import math
 
+tf = lambda wordFreq, maxFreq: wordFreq / maxFreq
 class Vector:
     def __init__(self, indices=[], values=[]):
-        self.indices = indices
-        self.values = values
+        self.tf_idf = {}
         self.words = {}
+        self.maxFrequency = 0
         
     def cosine(self, other):
         pass
@@ -17,3 +20,23 @@ class Vector:
         if self.words.get(word) is None:
             self.words[word] = 0
         self.words[word] += 1
+        self.maxFrequency = max(self.words[word], self.maxFrequency)
+        
+    def calcTfIdf(self, n, docFrequencies):
+        entries = self.words.items()
+        for word, freq in entries:
+            if docFrequencies[word] == 1:
+                del docFrequencies[word]
+            else:
+                self.tf_idf[word] = tf(freq, self.maxFrequency) * math.log2(n / docFrequencies[word])      
+    
+    def removeWord(self, word):
+        if word not in self.words:
+            return
+        del self.words[word]
+        self.total -= 1
+        
+    def outputSparseVector(self):
+        values = list(self.tf_idf.values())
+        words = list(self.tf_idf.keys())
+        return f'{",".join(words)}|{",".join([str(v) for v in values])}'
--- textVectorizer.py
+++ textVectorizer.py
@@ -9,39 +9,77 @@ import re
 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
+stopWords = set(stopwords.words('english'))
+
 
 sno = nltk.stem.PorterStemmer()
 
-preVectorizeAddWord = lambda vector, total: lambda word: addWord(word, vector, total)
+preVectorizeAddWord = lambda vector, wordAdded, df: lambda word: addWord(word, vector, wordAdded, df)
 
-def addWord(word, vector, total):
+def addWord(word, vector, wordAdded, df):
+    if word.lower() in stopWords:
+        return
+    
     word = sno.stem(word.lower())
-    if total.get(word) is None:
-        total[word] = len(total)
+    updateDocumentFrequency(df, word, wordAdded)
     vector.addWord(word)
+    
+def updateDocumentFrequency(df, word, wordAdded):
+    if df.get(word) is None:
+        df[word] = 0
+    if word not in wordAdded:
+        df[word] += 1
+        wordAdded.add(word)
 
-def vectorize(directory):
+def vectorize(directory, dataOut, truthOut):
     documentFiles = glob.glob(f'{directory}/*/*/*.txt')
-    docs = []
-    stopWords = set(stopwords.words('english'))
-    totalWords = {}
+    
+    vectors = []
+    authors = []
+    fileNames = []
+    documentFrequency  = {}
+    allVectors = {}
+    sparseOutPut = []
     for file in documentFiles:
+        wordCounted = set()
         with open(file, 'r') as fp:
             vector = Vector()
             name = os.path.basename(file.split('.')[0])
-            dir = os.path.dirname(file.split('.')[0])
+            author = os.path.dirname(file.split('.')[0]).split("\\")[2]
             text = " ".join(fp.readlines())
             # words = np.array(re.findall("[A-Z\-\']{2,}(?![a-z])|[A-Z\-\'][a-z\-\']+(?=[A-Z])|[\'\w\-]+", text.strip()))
             words = np.array(re.split("[\W]+", text))
-            words = [word for word in words if word.lower() not in stopWords]
-            np.vectorize(preVectorizeAddWord(vector, totalWords))(words)
-    print(len(totalWords))
+            np.vectorize(preVectorizeAddWord(vector, wordCounted, documentFrequency))(words)
+            vectors.append(vector)
+            authors.append(author)
+            fileNames.append(name)
+    for vector in vectors:
+        vector.calcTfIdf(len(documentFiles), documentFrequency)
+        sparseOutPut.append(vector.outputSparseVector())
+    for word in documentFrequency.keys():
+        allVectors[word] = [vector.tf_idf.get(word, 0) for vector in vectors]
+        
+    groundTruth = pd.DataFrame({"id": list(range(len(vectors))), "file": fileNames, "author": authors})        
+    vectorDataFrame = pd.DataFrame(allVectors)
+    
+    vectorDataFrame.to_csv(dataOut)
+    groundTruth.to_csv(truthOut)
+    
+    with open(f'sparse.out', 'w') as fp:
+        fp.writelines(sparseOutPut)
     
 def parse():
     parser = argparse.ArgumentParser(description="C45")
     parser.add_argument(
         "directory", type=str, help="directory of 50-50 dataset"
     )
+    parser.add_argument(
+        "--out", type=str, nargs='?', default='./data.out'
+    )
+    parser.add_argument(
+        "--truth", type=str, nargs='?', default='./truth.out'
+    )
+
 
     args = vars(parser.parse_args())
     return args
@@ -50,7 +88,9 @@ def parse():
 def main():
     args = parse()
     dataDirectory = args["directory"]
-    vectorize(dataDirectory)
+    dataOutputLoc = args['out']
+    truthOutputLoc = args['truth']
+    vectorize(dataDirectory, dataOutputLoc, truthOutputLoc)
     
 if __name__ == "__main__": 
     main()
